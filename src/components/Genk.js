19db68ffbb8da5ce309906eaccfc5a5ccd60514a
vukhanh09 <vxkhanh09@gmail.com>
2020-08-17 23:36:11 +0700

    update paper

--- src/components/Genk.js
+++ src/components/Genk.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './TopMenu.css'
-
+import {Products} from '../pages/root'
 
 
 import Web from './Web.js'
@@ -8,108 +8,8 @@ class Genk extends Component{
     constructor(props){
         super(props);
         this.state={
-          products:[{
-              "id": "d85b8d1d-3515-440f-8188-1ffc7d552bd5",
-              "name": "ssssdkdms6",
-              "description": "tusacsacsttis nibh ligula nec sem duis aliquam convallis nunc proin at turpis a pede posuere nonummy",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/dddddd/000000"
-            }, {
-              "id": "e40a6d8a-4514-4f79-9e01-ed7ac5a9df92",
-              "name": "Ginsing - Fresh",
-              "description": "in leo maecenas pulvinar lobortis est phasellus sit amet erat nulla tempus vivamus in felis eu sapien cursus",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/cc0000/ffffff"
-            }, {
-              "id": "aa3e00d1-cd42-44c3-bebb-a590c0cf370c",
-              "name": "Chips Potato Swt Chilli Sour",
-              "description": "justo eu massa donec dapibus duis at velit eu est congue elementum in hac habitasse",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/5fa2dd/ffffff"
-            }, {
-              "id": "b04eecfc-ef54-4567-8e24-194ed3339150",
-              "name": "Alize Red Passion",
-              "description": "vestibulum proin eu mi nulla ac enim in tempor turpis nec euismod scelerisque quam turpis adipiscing lorem vitae",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/ff4444/ffffff"
-            }, {
-              "id": "cc89afb4-f033-4ecd-a1cc-49253cb458d9",
-              "name": "Breadfruit",
-              "description": "orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula consequat morbi a ipsum",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/ff4444/ffffff"
-            }, {
-              "id": "073fe75e-081b-442b-bc33-a152a07517f7",
-              "name": "Salmon - Atlantic, Skin On",
-              "description": "lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien in",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/cc0000/ffffff"
-            }, {
-              "id": "44671ba3-110e-4d3b-bb89-e89ce4706900",
-              "name": "Soup - Campbells, Minestrone",
-              "description": "sollicitudin vitae consectetuer eget rutrum at lorem integer tincidunt ante vel ipsum praesent blandit lacinia erat vestibulum sed",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/cc0000/ffffff"
-            }, {
-              "id": "d44d7d66-acd2-4a9c-8c32-086503546c47",
-              "name": "Loaf Pan - 2 Lb, Foil",
-              "description": "augue a suscipit nulla elit ac nulla sed vel enim sit amet nunc viverra dapibus nulla suscipit ligula",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/5fa2dd/ffffff"
-            }, {
-              "id": "f1ea7c98-ad7f-4b37-9614-b8aa30209996",
-              "name": "Beer - Camerons Cream Ale",
-              "description": "eu sapien cursus vestibulum proin eu mi nulla ac enim in tempor turpis nec euismod scelerisque",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/dddddd/000000"
-            }, {
-              "id": "a0a6aaa5-d4c4-411b-b167-5db8b0c80436",
-              "name": "Veal - Sweetbread",
-              "description": "sed tincidunt eu felis fusce posuere felis sed lacus morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed nisl",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/cc0000/ffffff"
-            }, {
-              "id": "ce689a63-091c-4d39-9d1a-8042d8a8c76b",
-              "name": "Celery Root",
-              "description": "vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae donec pharetra magna vestibulum",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/ff4444/ffffff"
-            }, {
-              "id": "634c51d3-0e79-4e3d-965f-f2028b94611e",
-              "name": "Beans - Fava, Canned",
-              "description": "tempus semper est quam pharetra magna ac consequat metus sapien ut nunc vestibulum ante ipsum primis in faucibus",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/dddddd/000000"
-            }, {
-              "id": "4a15f49d-2308-46ff-9b2a-d5f6056d7bc0",
-              "name": "Nestea - Ice Tea, Diet",
-              "description": "porttitor lacus at turpis donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/ff4444/ffffff"
-            }, {
-              "id": "b40f0cc2-4f61-4bb1-84ee-5188e8000974",
-              "name": "Tea - Darjeeling, Azzura",
-              "description": "sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis in faucibus orci luctus et ultrices",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/cc0000/ffffff"
-            }, {
-              "id": "c3e0d489-9b86-4f8e-88ed-8524e1286151",
-              "name": "Muffin Mix - Carrot",
-              "description": "suspendisse potenti nullam porttitor lacus at turpis donec posuere metus vitae ipsum aliquam non mauris morbi",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/ff4444/ffffff"
-            }, {
-              "id": "b853ae56-8aa0-452b-8451-d5eec4cee789",
-              "name": "Cup - 4oz Translucent",
-              "description": "velit vivamus vel nulla eget eros elementum pellentesque quisque porta volutpat erat quisque erat eros viverra eget congue",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/5fa2dd/ffffff"
-            }, {
-              "id": "9a06000b-118d-4c81-9e56-7462daee85a1",
-              "name": "Rice Wine - Aji Mirin",
-              "description": "nullam varius nulla facilisi cras non velit nec nisi vulputate nonummy maecenas tincidunt lacus at velit vivamus vel nulla",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/dddddd/000000"
-            }, {
-              "id": "6067a5e7-365c-41be-92a0-8452edf8acda",
-              "name": "Coffee - Dark Roast",
-              "description": "in quam fringilla rhoncus mauris enim leo rhoncus sed vestibulum sit amet cursus id turpis integer aliquet massa",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/5fa2dd/ffffff"
-            }, {
-              "id": "ad20814d-a2f4-4708-ac00-2135f2f7d152",
-              "name": "Scallops - Live In Shell",
-              "description": "ut suscipit a feugiat et eros vestibulum ac est lacinia nisi venenatis tristique fusce congue",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/dddddd/000000"
-            }, {
-              "id": "13a3085b-5b17-4c51-8620-18c04a2ed237",
-              "name": "Temperature Recording Station",
-              "description": "tellus semper interdum mauris ullamcorper purus sit amet nulla quisque arcu libero rutrum ac lobortis vel dapibus",
-              "imageUrl": "http://dummyimage.com/300x200.jpg/dddddd/000000"
-            }]
-          }
+          products:Products
+        }
         
     }
     render(){
--- src/login/login.js
+++ src/login/login.js
@@ -8,18 +8,19 @@ export default class Login extends Component{
         return(
            <div>
                 <div className='login-box'>
-                <h1>Login</h1>
-                <div className='textbox'>
-                <i class="fa fa-user" aria-hidden="true"></i>
-                    <input type="text" placeholder="Username" />
-                </div>
-                
+                    <h1>Login</h1>
+                    <div className='textbox'>
+                    <i class="fa fa-user" aria-hidden="true"></i>
+                        <input type="text" placeholder="Username" />
+                    </div>
+                    
 
-                <div className='textbox'>
-                <i class="fa fa-lock" aria-hidden="true"></i>
-                    <input type="password" placeholder="Password" />
-                </div>
-                <input type="button" className="btn" value="Sign in"/>
+                    <div className='textbox'>
+                    <i class="fa fa-lock" aria-hidden="true"></i>
+                        <input type="password" placeholder="Password" />
+                    </div>
+                    
+                    <input type="button" className="btn" value="Sign in"/>
                  </div>
 
                 
